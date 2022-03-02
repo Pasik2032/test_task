d264d59022d1e47bd0cb21395e3f15f6018cf120
Pasiletskiy Daniil <dopasiletskiy@edu.hse.ru>
2022-03-02 22:34:54 +0300

    Initial Commit

---
+++ test task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test task
+//
+//  Created by Даниил Пасилецкий on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
